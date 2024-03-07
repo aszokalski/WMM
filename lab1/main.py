d4bf5b6507138f73885cd41b7b9d46489a5df05d
Adam Szokalski <szokalskiadam@gmail.com>
2024-03-07 18:45:05 +0100

    zad2

--- lab1/main.py
+++ lab1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def make_sin(pulsation):
-    return (lambda t: np.sin(pulsation, t)), 2 * np.pi / pulsation
+    return (lambda t: np.sin(pulsation * t)), 2 * np.pi / pulsation
 
 
 def sample_signal(signal, period, num_samples):
@@ -10,25 +10,28 @@ def sample_signal(signal, period, num_samples):
     dirac_d = np.arange(0, 1, delta_t)
     return signal(dirac_d), dirac_d
 
-
-def plot(signal, period, sample, dirac_d):
-    X_cont = np.arange(0, period)
-    X_discr = dirac_d * period
-
-    fig = plt.figure()
-    ax = fig.add_subplot()
-
-    ax.plot(X_cont, signal(X_cont), "-")
-    ax.plot(X_discr, sample, "o")
-
-
-
-
-
 def main():
     sin, period = make_sin(2 * np.pi)
-    sample, dirac_d = sample_signal(sin, period, 8)
-    plot(sin, period, sample, dirac_d)
+    samples, dirac_d = sample_signal(sin, period, 8)
+    spectrum_sin = np.fft.fft(samples)
+    freq_spectrum_sin = np.abs(spectrum_sin)
+    phase_spectrum_sin = np.angle(spectrum_sin)
+
+    X_cont = np.arange(0, period, 0.01)
+    X_discr = dirac_d * period
+
+    fig, ax = plt.subplots(1, 3)
+
+    ax[0].plot(X_cont, sin(X_cont), "--", color="red")
+    ax[0].stem(X_discr, samples, linefmt='k-', markerfmt='o', basefmt='k-')
+    ax[0].plot(X_cont, np.zeros(X_cont.shape), color="black")  # line
+
+    ax[1].stem(X_discr, freq_spectrum_sin)
+
+    ax[2].stem(X_discr, phase_spectrum_sin)
+
+    plt.show()
+
 
 
 if __name__ == "__main__":
